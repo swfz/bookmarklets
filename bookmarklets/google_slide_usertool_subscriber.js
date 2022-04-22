6b5cda747c5469e4c5492eaa824d3cea525fc0e0
swfz <sawafuji.09@gmail.com>
2022-04-22 16:27:15 +0900

    fix: selector

--- bookmarklets/google_slide_usertool_subscriber.js
+++ bookmarklets/google_slide_usertool_subscriber.js
@@ -1,14 +1,14 @@
 const broadcastChannel = new BroadcastChannel('comment_channel');
 
 const extractComment = (mutationRecords) => {
-  const nodes = mutationRecords.filter(record => record.target.className === 'punch-viewer-speaker-questions').map(record => record.addedNodes[0]);
+  const nodes = mutationRecords.filter(record => record.target.className === 'punch-ask-question-questions-list').map(record => record.addedNodes[0]);
   console.log(nodes);
   const comments = Array.from(nodes).map(node => node.children[1].children[2].innerText);
   console.log(comments);
   return comments;
 }
 
-const observeElement = document.querySelector('.punch-viewer-speaker-questions');
+const observeElement = document.querySelector('.punch-ask-question-questions-list');
 const observer = new MutationObserver(function(records) {
   console.log(records);console.log('callback that runs when observer is triggered');
   broadcastChannel.postMessage(extractComment(records));
